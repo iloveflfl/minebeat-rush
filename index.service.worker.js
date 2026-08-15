8619e384c36507a0befe38f336512481c693b5e6
iloveflfl <gomgonegi2@gmail.com>
2026-08-15 23:55:58 +0900

    Deploy v0.3.4

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786804151|2941990';
+const CACHE_VERSION = '1786805754|2994045';
 /** @type {string} */
 const CACHE_PREFIX = 'MineBeat Rush-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
